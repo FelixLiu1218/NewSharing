e0d25ad55374d0b56923c990152f8232f1800804
Felix <s80211394@gmail.com>
2022-02-14 09:59:19 +0900

    加入專案檔案。

---
+++ Sharing.Core/ApiModels/Contacts/SearchUsersResultsApiModel.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace Sharing.Core
+{
+    /// <summary>
+    /// The results of searching for users with specific details
+    /// </summary>
+    public class SearchUsersResultsApiModel : List<SearchUsersResultApiModel>
+    {
+
+    }
+}
